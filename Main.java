2f6f12f20f725c77d005a4f599f53d1f26037a65
RoaaAbeery <148685929+RoaaAbeery@users.noreply.github.com>
2023-10-26 08:37:54 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//    Q1
+//        for (int i = 1; i <=100 ; i++) {
+//
+//       if (i%5==0&&i%3==0){
+//           System.out.println("FizzBuzz" );
+//            } else if (i%3==0) {
+//           System.out.println("Fizz" );}
+//       else if (i%5==0) {
+//           System.out.println("Buzz");
+//       }
+//else  System.out.println(i);
+//        }
+//             Q2
+//        Scanner s2=new Scanner(System.in);
+//            System.out.println("Enter the string :");
+//        String st=s2.nextLine();
+//        int stl=st.length();
+//while(stl>0){
+//        System.out.print(st.charAt(stl-1));
+//        stl--;
+//}             Q3
+//        Scanner s3=new Scanner(System.in);
+//        System.out.println("Enter the number:");
+//        int num=s3.nextInt();
+//        int  fact =1;
+//        int n=num;
+//        while (num!=0){
+//            fact=fact *num;
+//            num=num-1;
+//        }
+//        System.out.printf("the factorial for " + n + "="+fact);
+//
+
+//            Q4
+//        Scanner s4=new Scanner(System.in);
+//        System.out.println("enter the number 1 ");
+//        int num1=s4.nextInt();
+//        System.out.println("enter the number 2 ");
+//        int num2=s4.nextInt();
+//        int pow=1;
+//        if(num2>=1){
+//            for (int i = 1; i <=num2 ; i++) {
+//                pow=pow*num2;
+//
+//            }
+//            System.out.println(pow);
+//        }
+
+//        Q5
+//                Scanner s5 = new Scanner(System.in);
+//
+//                System.out.print("Enter a set of integers (separated by spaces): ");
+//                String set = s5.nextLine();
+//
+//                Scanner intSet = new Scanner(set);
+//
+//                int sumEven = 0;
+//                int sumOdd = 0;
+//
+//                while (intSet.hasNext()) {
+//                    int number = intSet.nextInt();
+//
+//                    if (number % 2 == 0) {
+//
+//                        sumEven += number;
+//                    } else {
+//
+//                        sumOdd += number;
+//                    }
+//                }
+//
+//                System.out.println("Sum of even integers: " + sumEven);
+//                System.out.println("Sum of odd integers: " + sumOdd);
+
+//                      Q6
+//
+//                Scanner scanner = new Scanner(System.in);
+//
+//                System.out.print("Enter a positive integer: ");
+//                int num = scanner.nextInt();
+//
+//                if (num <= 1) {
+//                    System.out.println("Not a prime number.");
+//                } else {
+//                    boolean isPrime = true;
+//                    for (int i = 2; i < num; i++) {
+//                        if (num % i == 0) {
+//                            isPrime = false;
+//                            break;
+//                        }
+//                    }
+//
+//                    if (isPrime) {
+//                        System.out.println(num + " is a prime number.");
+//                    } else {
+//                        System.out.println(num + " is not a prime number.");
+//                    }
+//                }
+
+
+//        Q7
+//        int totalWeeks = 4;
+//        int daysPerWeek = 7;
+//
+//        for (int week = 1; week <= totalWeeks; week++) {
+//            System.out.println("Week " + week);
+//            for (int day = 1; day <= daysPerWeek; day++) {
+//                System.out.println("Day" + day);
+//            }
+//        }
+
+
+//        Q8
+
+//        String str, rev = "";
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter a string:");
+//        str = sc.nextLine();
+//
+//        int length = str.length();
+//
+//        for ( int i = length - 1; i >= 0; i-- )
+//            rev = rev + str.charAt(i);
+//
+//        if (str.equals(rev))
+//            System.out.println(str+" is a palindrome");
+//        else
+//            System.out.println(str+" is not a palindrome");
+            }
+        }
+
+
+
+
+
